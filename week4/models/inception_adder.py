1e17bb7fcab0179a9d27e60a78e695f9e90d7206
seenu-g <srinivasan.gsvasan@gmail.com>
2020-08-23 21:36:35 +0530

    Add files

---
+++ week4/models/inception_adder.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.optim import lr_scheduler
+from torch.nn.init import *
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
+class normalize(nn.Module):
+    def __init__(self):
+        super(normalize, self).__init__()
+        
+    def forward(self, x):
+        x = F.normalize(x, p=2, dim=1)
+        return x
